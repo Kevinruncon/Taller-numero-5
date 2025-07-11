602c4a6b36739f5c2da04e6c4ba14ee6954f7813
kevin rincon <168391474+Kevinruncon@users.noreply.github.com>
2025-07-11 17:23:27 -0500

    Add files via upload

---
+++ actividadJavaSwing/src/main/java/com/mycompany/actividadjavaswing/ActividadJavaSwing.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividadjavaswing;
+
+import vistas.VentanaPrincipal;
+import vistas.VentanaSecundaria;
+
+/**
+ *
+ * @author Kevin
+ */
+public class ActividadJavaSwing {
+
+    public static void main(String[] args) {
+    // crar ventana principal
+    new VentanaPrincipal();
+    
+    // mostrar la ventana secuendaria automaticamente 
+
+    }
+}
